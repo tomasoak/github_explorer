174afa956adda2628b05326dd0f0a75ba4b59d8d
Tomás Carvalho <tomas.jpeg@gmail.com>
2020-05-16 12:48:43 -0300

    app finished 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-// eslint-disable-next-line import/no-unresolved
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
